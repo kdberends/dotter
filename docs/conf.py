9aac7fbfc366f4e481f59bfa18a96f645bfcb437
Koen <koen.berends@deltares.nl>
2018-01-13 15:09:37 +0100

    doc rst to md

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.md'
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
